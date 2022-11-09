7aa0f265fd8a50afade83d80ce6c1ab71f306fd4
Leon's Laptop <102eon@gmail.com>
2022-11-09 13:44:27 -0800

    editing array elements exercise

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,30 @@
-for (let index = 0; index < 11; index++) {
-    console.log(2**(index))
-}
+// for (let index = 0; index < 11; index++) {
+//     console.log(2**(index))
+// }
 
-let i = 1
-while (i<11) {
-    console.log(2**i)
-    i++
+// let i = 1
+// while (i<11) {
+//     console.log(2**i)
+//     i++
+// }
+
+let input;
+do{
+input = prompt('what is your name?'); //always returns string
+console.log(Number(input));
 }
+while(isNaN(input) != true); // if user enters a number the result of Number() is a number, else it is NaN
+
+
+//alert('hello '+input)
+alert(`hello ${input}`)
+
+let fruits = ['Apple', 'Banana'];
+fruits.push('Orange');
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.unshift('Mango');
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
